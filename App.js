533fa712d0a0bae04cb3fcb2c9cba099dd12a7b3
Joulence <4erviken@gmail.com>
2023-08-11 15:30:15 +0300

    Adding first elements

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!!!!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View>
+        <TextInput placeholder="Your goal!"/>
+        <Button title="Add Goal"/>
+      </View>
+      <View>
+        <Text>List of goals...</Text>
+      </View>
+      <View></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  appContainer: {
+    padding: 50
+  }
 });
